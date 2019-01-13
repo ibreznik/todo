69a70cf2bedcce8cab932af4ff8a6ba90563f1d4
Ilya Reznik <reznik@Ilyas-MacBook-Pro.local>
2019-01-12 22:35:08 -0700

    Updates

---
+++ precache-manifest.ba9eb3a262ec076af31b49b715dba5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "76f5c0df21899f0e9a35",
+    "url": "/todo/static/js/main.76f5c0df.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/todo/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "76f5c0df21899f0e9a35",
+    "url": "/todo/static/css/main.365154c3.chunk.css"
+  },
+  {
+    "revision": "87b103be9ed2454c95d4118375ccbf29",
+    "url": "/todo/index.html"
+  }
+];
